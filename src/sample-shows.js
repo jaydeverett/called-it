509e444c83df8f219d9de59b7464f18e4c11de78
Jay Deverett <jayjdeverett@gmail.com>
2017-10-17 14:43:50 -0400

    add 'predictions' param to sample data

--- src/sample-shows.js
+++ src/sample-shows.js
@@ -3,62 +3,71 @@ module.exports = {
     name: 'Game of Thrones',
     image: 'https://image.tmdb.org/t/p/original/gwPSoYUHAKmdyVywgLpKKA4BjRr.jpg',
     desc: 'Nine noble families fight for control over the mythical lands of Westeros, while a forgotten race returns after being dormant for thousands of years.',
-    seasons: 7
+    seasons: 7,
+    predictions: {}
   },
 
   show2: {
     name: 'Rick and Morty',
     image: 'https://image.tmdb.org/t/p/original/qJdfO3ahgAMf2rcmhoqngjBBZW1.jpg',
     desc: 'An animated series that follows the exploits of a super scientist and his not-so-bright grandson.',
-    seasons: 4
+    seasons: 4,
+    predictions: {}
   },
 
   show3: {
     name: 'Stranger Things',
     image: 'https://image.tmdb.org/t/p/original/esKFbCWAGyUUNshT5HE5BIpvbcL.jpg',
     desc: 'When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.',
-    seasons: 1
+    seasons: 1,
+    predictions: {}
   },
 
   show4: {
     name: 'Riverdale',
     image: 'https://image.tmdb.org/t/p/original/1TsbOTztAJtzTRXAhoLsX9a83XX.jpg',
     desc: 'A subversive take on Archie and his friends, exploring small town life, the darkness and weirdness bubbling beneath Riverdales wholesome facade.',
-    seasons: 2
+    seasons: 2,
+    predictions: {}
   },
 
   show5: {
     name: 'Westworld',
     image: 'https://image.tmdb.org/t/p/original/x2WKIbiwhLoWgLFbT2I0Gwq8U1J.jpg',
     desc: 'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',
-    seasons: 1
+    seasons: 1,
+    predictions: {}
   },
 
   show6: {
     name: 'Ozark',
     image: 'https://image.tmdb.org/t/p/original/pCGyPVrI9Fzw6rE1Pvi4BIXF6ET.jpg',
     desc: 'A Chicago-based financial advisor secretly relocates his family to the Missouri Ozarks when his dealings with a drug cartel go awry.',
-    seasons: 1
+    seasons: 1,
+    predictions: {}
   },
 
   show7: {
     name: 'Legion',
     image: 'https://image.tmdb.org/t/p/original/zvyA5E4YxQw79gX0hzZ2pQKJpKy.jpg',
     desc: 'David Haller is a troubled young man diagnosed as schizophrenic, but after a strange encounter, he discovers special powers that will change his life forever.',
-    seasons: 1
+    seasons: 1,
+    predictions: {}
   },
 
   show8: {
     name: 'Bojack Horseman',
     image: 'https://image.tmdb.org/t/p/original/1bnrSsJNejoQq8lGBDECQroGjPz.jpg',
     desc: 'BoJack Horseman was the star of the hit TV show "Horsin Around" in the 90s, now hes washed up, living in Hollywood, complaining about everything, and wearing colorful sweaters.',
-    seasons: 4
+    seasons: 4,
+    predictions: {}
   },
 
   show9: {
     name: 'American Gods',
     image: 'https://image.tmdb.org/t/p/original/gevw5nZRYz2kWj1PqW9pz4sgeeZ.jpg',
     desc: 'A recently released ex-convict named Shadow meets a mysterious man who calls himself "Wednesday" and who knows more than he first seems to about Shadows life and past.',
-    seasons: 1
+    seasons: 1,
+    predictions: {}
   }
 };
